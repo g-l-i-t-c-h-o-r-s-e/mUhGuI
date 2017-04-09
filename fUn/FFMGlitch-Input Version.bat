1c776b6c836951d865561734178ea30e9b71046d
g-l-i-t-c-h-o-r-s-e <anne.hero88@gmail.com>
2017-04-09 06:58:14 -0600

    OH SHIT WADDUP
    The Beginning.

---
+++ fUn/FFMGlitch-Input Version.bat
@@ -0,0 +1,150 @@
+@Echo Off
+color 0d
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+echo h̴̨̜̣̫͔̫̲̥̱͋̍͊͊͐̈́͐̄̒͊͜ọ̷̡͕̤̩̖̤̠̖̰̽̄͛̎̄̆̂́̒͘ṟ̷̟̝̲̟̗̗̝̗̍̇͒͑̏̂̔̓̅̊͜s̸̭̭̼̰̜͖͚̬͇̪̏͑͒̿̅͐̒̇̋̌e̴͍̝͎̞͔͈̙̟͉̪͆́̊́̐̈́͂̀̈͐p̷̧͎̭͈̠͚̰̰̣̞̒́̈̑͂̓̈́̍͘͠ǘ̵̢͉̥͕̩̳̟͍̜͔͌͑͑̏̈̀̈́̈̆s̴̨̥͇̙̬̟̩͚̲͙̏̽̂͐́̓̾̌͘̚ṡ̴̢͎̳̜̰̞̞̩̩̮̿͗́͗̓̊̅̕͝y̵̢̨̨̧̮̖̗̲̲̪̩̜̜̣̫͔̫̲̥̱̰̰̒͆̍̉̅͌̓̏͛͊̍͊͊͐̈́͐̄̒͊̒̒͊͜
+title kms
+call :ColorText 0a "I"
+set /p ".= " <nul
+call :ColorText 0c "WANT"
+set /p ".= " <nul
+call :ColorText 09 "TO"
+set /p ".= " <nul
+call :ColorText 0d "BE"
+set /p ".= " <nul
+call :ColorText 0e "THE"
+set /p ".= " <nul
+call :ColorText 0a "LITTLE"
+set /p ".= " <nul
+call :ColorText 0c "HORSE"
+set /p ".= " <nul
+call :ColorText 09 "THAT"
+set /p ".= " <nul
+call :ColorText 0d "WAS"
+set /p ".= " <nul
+call :ColorText 0e ","
+set /p ".= " <nul
+call :ColorText 0a "AND"
+set /p ".= " <nul
+call :ColorText 0c "IS"
+set /p ".= " <nul
+call :ColorText 09 ","
+set /p ".= " <nul
+call :ColorText 0d "AND"
+set /p ".= " <nul
+call :ColorText 0e "IS"
+set /p ".= " <nul
+call :ColorText 0a "TO"
+set /p ".= " <nul
+call :ColorText 09 "C O M E" end
+echo.
+call :colorEcho 08  "( Glitch The Fuck Out Of Your Favorite Cartoon Or Something Rad Like That )"
+echo.
+echo **************************************************************
+call :colorEcho 0a  "UNCOMPRESSED VIDEO ENCODERS" 
+echo.
+echo.
+call :colorEcho 2f "v410, v408, v308, v210, r120, ayuv, avui, y41p yuv4, rawvideo"
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+call :colorEcho 0a  "UNCOMPRESSED VIDEO DECODERS"
+echo.
+echo.
+call :colorEcho 2f "012v, avui, ayuv, r210, v210x, y41p, yuv4, frwu, m101, rawvideo"
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+call :colorEcho 09 "COMPRESSED VIDEO ENCODERS"
+echo.
+echo.
+call :colorEcho 2f "msvideo1, cljr, utvideo, cavs, r10k, asv1, asv2, avrp, pgmyuv"
+echo.
+call :colorEcho 2f "cinepak, mpeg1video, mpeg2video, h261, bmp, targa, tiff, zlib"
+echo.
+call :colorEcho 2f "snow, vc2, libschroedinger, dnxhd, "
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+call :colorEcho 09 "COMPRESSED VIDEO DECODERS"
+echo.
+echo.
+call :colorEcho 2f "vcr1, vp3, msvideo1, cljr, bethsoftvid, kgv1, motionpixels"
+echo.
+call :colorEcho 2f "avrn, avrp, avs, bfi, dirac, libschroedinger, dvvideo, cyuv"
+echo.
+call :colorEcho 2f "escape124, tmv, "
+echo.
+echo **************************************************************
+echo.
+call :colorEcho 4e  "check ffmpeg.exe -codecs for more"
+echo.
+echo.
+call :colorEcho 19 "     -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-             "
+echo.
+call :colorEcho 0d                "                   Alright lets glitch some shit"
+echo.
+call :colorEcho 0d   "     Uncompressed Encoder & Decoder combinations work the best FYI"
+echo.
+call :colorEcho 0d "  Some Codecs I found work together, some require specific colour formats"
+echo.
+title A R O U N D  T H E  W O R L D  A R O U N D  T H E  W O R L D  A R O U N D  T H E  W O R L D  A R O U N D  T H E  W O R L D  A R O U N D  T H E  W O R L D A R O U N D  T H E  W O R L D  A R O U N D  T H E  W O R L D
+echo.
+call :ColorText 2f "   v410"
+set /p ".= " <nul
+call :ColorText 4e "   r10k"
+set /p ".= " <nul
+call :ColorText 2f "   v408"
+set /p ".= " <nul
+call :ColorText 4e "   ayuv"
+set /p ".= " <nul
+call :ColorText 2f "   avrp" END
+echo.
+echo.
+call :colorEcho 0e " cljr   ayuv   012v   v210x   r210"
+echo.
+call :colorEcho 0e " r10k   v210   kgv1   r10k    ayuv"
+echo.
+call :colorEcho 0e " vcr1   kgv1   r10k   r210    v408"
+echo.
+call :colorEcho 0e " ayuv   avrp   avrp   v408    cljr"
+echo.
+call :colorEcho 0e " yuv4   012v   msvideo1       vp3"
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+call :colorEcho 4e "To see more colour formats use ffmpeg.exe -pix_fmts"
+echo.
+echo.
+echo.
+echo.
+echo.
+call :colorEcho 19 "     -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-             "
+echo.
+echo.
+echo.
+set /p preoption= 8D ~ uR pReOpTiOn pLs [-re, -ss 00:13:37] :
+set /p leinput= 8=D ~ uR iNpUt pLs [https://goo.gl/qOlRQ2] :
+set /p vidcodeca= 8==D ~ uR eNc0dEr pLs [rawvideo, V408, ayuv, r10k, etc] :
+set /p vidcodecAoptions= 8===D ~ uR 0pTi0nAlS pLs (can leave blank) [-b:v 888k, -q 31 -bf 0, etc] :
+set /p colorformata= 8====D ~ uR c0l0Rs pLs [yuv420p, pal8, bgr0, gbrap, etc] :
+set /p resolution= 8=====D ~ uR rEz0lUtI0Nz pLs [1280x720, 1920x1080, 666x666, etc] :
+set /p vidcodecb= 8======D ~ uR dEc0deR pLs [rawvideo, v408, ayuv, etc] :
+set /p colorformatb= 8=======D ~ uR dEc0deRs c0l0rS pLs [yuv420p, pal8, bgr4_byte, gbrap, etc] :
+set /p vidcodecBoptions= 8========D ~ uR 0tHeR 0pTi0nAls (can leave blank) [-f rawvideo -s 1280x720, etc] :
+echo W H A T  T H E  F U C K  D I D  Y O U  J U S T  S A Y  A B O U T  M E  Y O U  L I T T L E  B I T C H
+ffmpeg %preoption% -i %leinput% -vcodec %vidcodeca% %vidcodecAoptions% -vf format=%colorformata% -s %resolution% -f avi - | ffplay -i - -vcodec %vidcodecb% -vf format=%colorformatb% %vidcodecBoptions%
+pause
+EXIT /B
+Echo.
+Echo.
+:colorEcho
+:ColorText [%1 = Color] [%2 = Text]
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1
